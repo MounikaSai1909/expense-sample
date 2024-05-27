91254758190ef0b83a177deb7ac272d34835e4b5
MounikaSai1909 <mounikasaiaarepu09@gmail.com>
2024-05-27 11:05:25 +0530

    some-message

--- mysql.sh
+++ mysql.sh
@@ -34,4 +34,7 @@ VALIDATE $? "Enabling MySQL Server"
 
 systemctl start mysqld &>> $LOGFILE
 VALIDATE $? "Starting MySQL Server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+VALIDATE $? "Setting up password"
     
